2e2d45515d44682e99ee4b6a0bcb1eafb55cad16
Fsociety-Mrn <lisboamillen30@gmail.com>
2022-09-28 17:36:04 +0800

    add a vb net software

--- Cognate1/Cognate1/Form1.Designer.vb
+++ Cognate1/Cognate1/Form1.Designer.vb
@@ -22,29 +22,42 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'Button1
+        'ComboBox1
         '
-        Me.Button1.Location = New System.Drawing.Point(222, 153)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(12, 52)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 1
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(162, 44)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(223, 284)
+        Me.DataGridView1.TabIndex = 2
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(532, 371)
-        Me.Controls.Add(Me.Button1)
+        Me.ClientSize = New System.Drawing.Size(693, 371)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.ComboBox1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
-
-    Friend WithEvents Button1 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
 End Class
--- Cognate1/Cognate1/Form1.vb
+++ Cognate1/Cognate1/Form1.vb
@@ -1,5 +1,59 @@
-﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+﻿Imports MySql.Data.MySqlClient
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
         MessageBox.Show("Hello Friend")
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Try
+            openCon() ''connection 
+
+            mysqlCommand.Connection = con
+            mysqlCommand.CommandText = "SHOW TABLES;"
+            mysqlAdapter.SelectCommand = mysqlCommand
+
+            data.Clear() ''dataset
+            mysqlAdapter.Fill(data)
+
+            Dim myread As MySqlDataReader = mysqlCommand.ExecuteReader
+
+
+            While myread.Read
+                ComboBox1.Items.Add(myread(0))
+            End While
+
+            con.Close()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString)
+        End Try
+
+    End Sub
+
+
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+
+        Dim bindingSource As New BindingSource
+        Dim dataTable As New DataTable
+
+        Try
+            openCon() ''connection 
+
+            mysqlCommand.Connection = con
+            mysqlCommand.CommandText = "SELECT * FROM `" + ComboBox1.Text + "`;"
+            mysqlAdapter.SelectCommand = mysqlCommand
+
+            mysqlAdapter.Fill(dataTable)
+            bindingSource.DataSource = dataTable
+            DataGridView1.DataSource = bindingSource
+            mysqlAdapter.Update(dataTable)
+
+            con.Close()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString)
+        End Try
     End Sub
 End Class
